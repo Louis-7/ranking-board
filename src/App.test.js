286352b4e142518d6709dcc0f4fb947d985f02a9
Louis Liu <66584553+louis-bec@users.noreply.github.com>
2022-08-02 17:13:21 +0800

    [fix] Fix auto deploy issues
    - Fix craco 6.x not support CRA 5.x issue. Use alpha version instead.
    - Fix jtest failed issue. Use a workaround.

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders app title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Ranking/i);
+  const linkElement = screen.getByText(/Ranking Board/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -4,3 +4,15 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 import 'jest-canvas-mock';
+
+// Kill matchMedia not found issue. The workaround in the jtest official document is not working,
+// so I found another solution in the antd issues on GitHub.
+// Ref: https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+// Ref: https://github.com/ant-design/ant-design/issues/21096#issuecomment-725301551
+global.matchMedia = global.matchMedia || function () {
+    return {
+      matches: false,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+    };
+  };
