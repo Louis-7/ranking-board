d767a8e0f2900c3b93a15824070ecfbed436f36c
Louis-7 <louisgh.cn@gmail.com>
2023-11-26 10:24:52 +0800

    feat: move ranking chart to the right place

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,11 @@
 
 import { useEffect, useState } from "react";
 import { Card, CardHeader, CardBody } from "@nextui-org/card";
-import { RankingChart } from "./home/ranking-chart";
 import { Events } from "@/services/events";
-import { RankingRecord } from "@/types/event-data";
 import { Skeleton } from "@nextui-org/skeleton";
 
 export default function Home() {
-  const [rankingRecords, setRankingRecords] = useState<RankingRecord[]>([]);
+
   const [stats, setStats] = useState({ totalEvents: 0, totalUsers: 0, totalPoints: 0 });
   const [isLoading, setIsLoading] = useState(true);
 
@@ -17,7 +15,6 @@ export default function Home() {
 
     eventService.loadEvents().then(() => {
       setIsLoading(false);
-      setRankingRecords(eventService.getRankingRecords());
       setStats({ totalEvents: eventService.getTotalEvents(), totalUsers: eventService.getTotalUsers(), totalPoints: eventService.getTotalPoints() });
     });
 
@@ -29,13 +26,16 @@ export default function Home() {
   return (
     <div className="grid grid-cols-1 gap-4">
       <div className="flex justify-center gap-x-6">
+
         <Card className="w-60">
           <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
             <p className="text-tiny uppercase font-bold">Total</p>
             <h4 className="font-bold text-large">Events</h4>
           </CardHeader>
           <CardBody className="overflow-visible py-2">
-            <h4 className="font-bold text-4xl">{stats.totalEvents}</h4>
+            <Skeleton isLoaded={!isLoading} className="flex rounded-lg" classNames={{ base: "dark:bg-transparent" }}>
+              <h4 className="font-bold text-4xl">{stats.totalEvents}</h4>
+            </Skeleton>
           </CardBody>
         </Card>
         <Card className="w-60">
@@ -44,7 +44,9 @@ export default function Home() {
             <h4 className="font-bold text-large">Users</h4>
           </CardHeader>
           <CardBody className="overflow-visible py-2">
-            <h4 className="font-bold text-4xl">{stats.totalUsers}</h4>
+            <Skeleton isLoaded={!isLoading} className="flex rounded-lg" classNames={{ base: "dark:bg-transparent" }}>
+              <h4 className="font-bold text-4xl">{stats.totalUsers}</h4>
+            </Skeleton>
           </CardBody>
         </Card>
         <Card className="w-60">
@@ -53,14 +55,11 @@ export default function Home() {
             <h4 className="font-bold text-large">Points</h4>
           </CardHeader>
           <CardBody className="overflow-visible py-2">
-            <h4 className="font-bold text-4xl">{stats.totalPoints}</h4>
+            <Skeleton isLoaded={!isLoading} className="flex rounded-lg" classNames={{ base: "dark:bg-transparent" }}>
+              <h4 className="font-bold text-4xl">{stats.totalPoints}</h4>
+            </Skeleton>
           </CardBody>
         </Card>
-      </div>
-      <div className="md:container lg:mx-auto md:mx-auto sm:mx-auto">
-        <Skeleton isLoaded={!isLoading} className="rounded-lg">
-          <RankingChart rankingRecords={rankingRecords} />
-        </Skeleton>
       </div>
     </div>
   )
--- app/ranking/page.tsx
+++ app/ranking/page.tsx
@@ -1,9 +1,36 @@
+'use client'
+import { useEffect, useState } from "react";
 import { title } from "@/components/primitives";
+import { Skeleton } from "@nextui-org/skeleton";
+import { RankingChart } from "./ranking-chart";
+import { RankingRecord } from "@/types/event-data";
+import { Events } from "@/services/events";
+
 
 export default function RankingPage() {
+	const [rankingRecords, setRankingRecords] = useState<RankingRecord[]>([]);
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+    const eventService = new Events();
+
+    eventService.loadEvents().then(() => {
+      setIsLoading(false);
+      setRankingRecords(eventService.getRankingRecords());
+    });
+
+    return () => {
+      // Cleanup code here
+    };
+  }, []);
+	
 	return (
-		<div>
-			<h1 className={title()}>Ranking</h1>
+		<div className="md:container lg:mx-auto md:mx-auto sm:mx-auto">
+			<div>
+        <Skeleton isLoaded={!isLoading} className="rounded-lg">
+          <RankingChart rankingRecords={rankingRecords} />
+        </Skeleton>
+      </div>
 		</div>
 	);
 }
